dde01f8445327ce76e3b4fe28bb708649c117b02
chimcuccu258 <vuminhnga25@gmail.com>
2023-11-21 08:21:36 +0700

    update

---
+++ ViewPager2TabLayoutFragment/app/src/main/java/vn/minhnga/viewpager2tablayoutfragment/CountryPageAdapter.java
@@ -0,0 +1,32 @@
+package vn.minhnga.viewpager2tablayoutfragment;/*
+ * @created 21/11/2023 08:14
+ * @author MinhNga
+ */
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.List;
+
+public class CountryPageAdapter extends FragmentStateAdapter {
+    List<Country> lstDataSource;
+
+    public CountryPageAdapter(@NonNull FragmentActivity fragmentActivity, List<Country> dataSource ) {
+        super(fragmentActivity);
+        lstDataSource = dataSource;
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        Country c = lstDataSource.get(position);
+        CountryFragment countryFragment = new CountryFragment(c);
+        return countryFragment;
+    }
+
+    @Override
+    public int getItemCount() {
+        return lstDataSource.size();
+    }
+}
